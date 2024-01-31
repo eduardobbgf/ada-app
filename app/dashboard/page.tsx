87bb60b0a4cff2565613a9f28754512fa2b14f23
eduardobbgf <eduardo.bbgf@gmail.com>
2024-01-31 11:23:48 -0300

    chore: add card style

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,6 +5,16 @@ import styles from "./page.module.css";
 
 export default function Dashboard() {
   return (
-    <main className={styles.main}>page</main>
+    <main className={styles.main}>
+      <div className={styles.container}>
+        <div className={styles.card}></div>
+        <div className={styles.card}></div>
+        <div className={styles.card}></div>
+        <div className={styles.card}></div>
+        <div className={styles.card}></div>
+        <div className={styles.card}></div>
+
+      </div>
+    </main>
   )
 }
